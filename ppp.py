7275ad2ba39918764ffa36efc56d26b68fd49415
zsdasdsad <82306828+zsdasdsad@users.noreply.github.com>
2021-05-16 12:31:18 +0500

    Add files via upload

--- ppp.py
+++ ppp.py
@@ -7,21 +7,34 @@ win = QWidget()
 
 l1 = QLabel("От:")
 l2 = QLabel("До:")
-l3 = QLabel("Результат:")
+l3 = QLabel("Список результатов:")
 
 ag = QPushButton("Сгенерировать")
+clearbut = QPushButton("Очистить список")
 tb2 = QLineEdit()
 tb3 = QLineEdit()
 fin = QListWidget()
+def avc():
+    a = int( tb2.text())
+    b = int( tb3.text())
+    if a<b:
+        rr = randint(a,b)
+        rr = str(rr)
+        fin.addItem(rr)
+    else:
+        fin.addItem("Неверный диаппзон чисел")
 
-#hline1 = QHBoxLayout()
+def clear():
+    fin.clear()
+
+clearbut.clicked.connect(clear)
+ag.clicked.connect(avc)
 hline2 = QHBoxLayout()
 hline3 = QHBoxLayout()
 hline4 = QHBoxLayout()
+hline5 = QHBoxLayout()
 
 winline = QVBoxLayout()
-
-#hline1.addWidget(ml)
 
 hline2.addWidget(l1)
 hline2.addWidget(tb2)
@@ -30,11 +43,13 @@ hline2.addWidget(tb3)
 hline3.addWidget(ag)
 hline4.addWidget(l3)
 hline4.addWidget(fin)
+hline5.addWidget(clearbut)
 
-#winline.addLayout(hline1)
 winline.addLayout(hline2)
 winline.addLayout(hline3)
 winline.addLayout(hline4)
+winline.addLayout(hline5)
+
 
 win.setLayout(winline)
 
